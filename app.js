3b3f75ad460d9f6b9d4780d12fce89affe6570ca
Vladislav <timaevvladislav@gmail.com>
2023-02-07 00:07:57 +0600

    init

--- app.js
+++ app.js
@@ -10,55 +10,6 @@ const bot = new TelegramBot(token, { polling: true })
 const configuration = new Configuration({ apiKey: process.env.OPENAI_TOKEN })
 const openai = new OpenAIApi(configuration)
 
-bot.on('message', async (message) => {
-   if(message.text !== "/start") {
-       const baseCompletion = await openai.createCompletion({
-           model: 'text-davinci-003',
-           prompt: `${message.text}.\n`,
-           temperature: 0.8,
-           max_tokens: 1000,
-       })
-
-       const chatId = message.chat.id
-
-       const basePromptOuput = baseCompletion.data.choices.pop()
-
-       if (!basePromptOuput.text) {
-           return bot.sendMessage(chatId, "Пожалуйста, попробуйте ещё раз, Бот не смог отправить данные...")
-       }
-
-       return bot.sendMessage(chatId, basePromptOuput.text)
-   }
-})
-bot.onText(/\/start/, async (message) => {
-    const chatId = message.chat.id
-
-    const options = {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    {text: 'Интересный факт', callback_data: '1'},
-                    {text: 'Расписание уроков', callback_data: '2'}
-                ]
-
-            ]
-        }
-    }
-    return bot.sendMessage(chatId, `Добро пожаловать в школьный бот! Я здесь, чтобы помочь вам со всеми вашими школьными потребностями.`, options)
-})
-
-bot.on('callback_query', (callbackQuery) => {
-    const action = callbackQuery.data
-    const message = callbackQuery.message
-    const chatId = message.chat.id
-
-    if (action) {
-        // Отловить нажатие на копку
-        return bot.sendMessage(chatId, "Пожалуйста, ожидайте, Я отправляю вам данные...")
-    }
-})
-
-
 
 const schedule = {
     '5': {
@@ -91,19 +42,71 @@ const schedule = {
     }
 }
 
-const handlerSendSchedule = () => {
-    bot.on('message', (msg) => {
-        const chatId = msg.chat.id
-        const classLetter = msg.text[0]
-        const classNumber = msg.text[1].toUpperCase()
 
-        if (schedule[classLetter] && schedule[classLetter][classNumber]) {
-            const subjects = schedule[classLetter][classNumber]
-            const scheduleMessage = `Расписание ${classLetter}${classNumber} класса: \n\n` + subjects.join('\n')
-            return bot.sendMessage(chatId, scheduleMessage)
-        } else {
-            return bot.sendMessage(chatId, 'Класс не найден. Пожалуйста, введите действительную букву класса и номер.')
+bot.on('message', async (message) => {
+    if(message.text !== "/start") {
+        const baseCompletion = await openai.createCompletion({
+            model: 'text-davinci-003',
+            prompt: `${message.text}.\n`,
+            temperature: 0.8,
+            max_tokens: 1000,
+        })
+
+        const chatId = message.chat.id
+
+        const basePromptOuput = baseCompletion.data.choices.pop()
+
+        if (!basePromptOuput.text) {
+            return bot.sendMessage(chatId, "Пожалуйста, попробуйте ещё раз, Бот не смог отправить данные...")
         }
+
+        return bot.sendMessage(chatId, basePromptOuput.text)
+    }
+})
+
+bot.onText(/\/start/, async (message) => {
+    const chatId = message.chat.id
+
+    const options = {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {text: 'Интересный факт', callback_data: '1'},
+                    {text: 'Расписание уроков', callback_data: '2'}
+                ]
+
+            ]
+        }
+    }
+    return bot.sendMessage(chatId, `Добро пожаловать в школьный бот! Я здесь, чтобы помочь вам со всеми вашими школьными потребностями.`, options)
+})
+
+bot.on('callback_query', (callbackQuery) => {
+    const action = callbackQuery.data
+    const message = callbackQuery.message
+    const chatId = message.chat.id
+
+    if (action) {
+        // Отловить нажатие на кнопку
+        return bot.sendMessage(chatId, "Пожалуйста, ожидайте, Я отправляю вам данные...")
+    }
+})
+
+const handlerSendSchedule = () => {
+    bot.on('message', (message) => {
+       if(message.text !== "/start") {
+           const chatId = message.chat.id
+           const classLetter = message.text[0]
+           const classNumber = message.text[1].toUpperCase()
+
+           if (schedule[classLetter] && schedule[classLetter][classNumber]) {
+               const subjects = schedule[classLetter][classNumber]
+               const scheduleMessage = `Расписание ${classLetter}${classNumber} класса: \n\n` + subjects.join('\n')
+               bot.sendMessage(chatId, scheduleMessage)
+           } else {
+               return bot.sendMessage(chatId, 'Класс не найден. Пожалуйста, введите действительную букву класса и номер.')
+           }
+       }
     })
 }
 
@@ -130,12 +133,15 @@ bot.on('callback_query', async (callbackQuery) => {
             await bot.sendMessage(chatId, basePromptOuput.text)
             break
         case '2':
-            await bot.sendMessage(chatId, 'Введите номер и букву вашего класса')
+            bot.sendMessage(chatId, 'Введите номер и букву вашего класса на английском')
             handlerSendSchedule()
             break
         default:
-           return bot.sendMessage(chatId, 'Invalid option selected')
+            return bot.sendMessage(chatId, 'Invalid option selected')
     }
 })
 
-
+module.exports = bot
+module.exports = token
+module.exports = TelegramBot
+module.exports = config()
